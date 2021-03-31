2caa1b9d2f9a595be2b46c23085204b3409ad15a
rishabh9352963684 <71339017+rishabh9352963684@users.noreply.github.com>
2021-03-31 08:21:36 +0530

    Create FerToCel.java

---
+++ FerToCel.java
@@ -0,0 +1,9 @@
+public class FerToCel 
+ {  
+   public static void main (String args[])  
+    { float f, c;  
+          f = 43;  
+          c  = ((F-32)*5)/9;  
+          System.out.println("Temperature in celsius is: "+c);  
+    }
+    }
