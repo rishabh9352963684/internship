3d5e1fba0b7ea257d2586c27ea9534aaebb81b67
rishabh9352963684 <71339017+rishabh9352963684@users.noreply.github.com>
2021-03-31 08:01:40 +0530

    Create SwapOne.java

---
+++ SwapOne.java
@@ -0,0 +1,11 @@
+public class SwapOne{
+   public static void main(String [] args){
+     int a,b;
+      a = 6, b = 8;
+      b = b-a
+      a = b+a
+      b = -(b-a)
+      System.out.println("After Swapping:"+a,+b)
+   }
+
+}
