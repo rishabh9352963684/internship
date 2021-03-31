2f656b792e4eeaa63e580be97945461a15500d38
rishabh9352963684 <71339017+rishabh9352963684@users.noreply.github.com>
2021-03-31 07:58:44 +0530

    Create Swap.java

---
+++ Swap.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Swap{
+    public static void main(String[] args){
+      int a, b, c;
+      a = 6;
+      b = 8;
+    a = c
+    a = b
+    b = c
+   System.out.println("After Swapping:"+a, +b);
+}
