a1f9c93057e70579d1211d35c3222190104f5163
rishabh9352963684 <71339017+rishabh9352963684@users.noreply.github.com>
2021-03-31 07:14:11 +0530

    Create First.java

---
+++ First.java
@@ -0,0 +1,8 @@
+class First{
+  public static void main(String[] args){
+    int x = 10;
+    double y = 10.23;
+    char z = 'z';
+    System.out.println("int : "+x+" float : "+y+" char : "+z);
+  }
+}
