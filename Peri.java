b13eab2504f43e8f4d3f93d5b54d727cd714f587
rishabh9352963684 <71339017+rishabh9352963684@users.noreply.github.com>
2021-03-31 07:31:46 +0530

    Create Peri.java

---
+++ Peri.java
@@ -0,0 +1,8 @@
+immport java.util.*;
+public class Peri{
+     public static void main(String[] args){
+    int x = 5
+    int y = 7
+    System.out.println("Area is "+(5*7)+"\nPerimeter is "+(2*(5+7)));
+  }
+}
