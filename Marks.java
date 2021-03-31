0bf4179903b60bf55cc02395f99ed998601cccbd
rishabh9352963684 <71339017+rishabh9352963684@users.noreply.github.com>
2021-03-31 08:10:58 +0530

    Create Marks.java

---
+++ Marks.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class percent{
+public static void main(String [] args)
+{
+int a=78;
+int b=45;
+int c=62;
+int sum=a+b+c;
+double per=(sum/300)*100;
+System.out.println(sum,per);
+}
+}
