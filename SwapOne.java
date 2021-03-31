6bd5d1df09b9317c7266d6a6dc0d28a9d550931d
rishabh9352963684 <71339017+rishabh9352963684@users.noreply.github.com>
2021-03-31 12:12:26 +0530

    Update SwapOne.java

--- SwapOne.java
+++ SwapOne.java
@@ -1,11 +1,14 @@
 public class SwapOne{
    public static void main(String [] args){
-     int a,b;
-      a = 6, b = 8;
-      b = b-a
-      a = b+a
-      b = -(b-a)
-      System.out.println("After Swapping:"+a,+b)
+     int a, b;
+      a = 6; 
+      b = 8;
+      b = b-a;
+      a = b+a;
+      b = -(b-a);
+      System.out.println("After Swapping a:"+a);
+      System.out.println("After Swapping b:"+b);
+
    }
 
 }
